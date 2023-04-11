dd3fe45cf21abb2930aec29bda38c6cfc22e451c
sasha <sasakorepin78140@gmail.com>
2023-04-11 19:07:50 +0300

    1

--- functions.py
+++ functions.py
@@ -2,52 +2,7 @@ import sqlite3
 
 
 
-# def get_practise_task(task_id=-1):
-#     try:
-#         sqlite_connection = sqlite3.connect('sqlite_python.db')
-#         cursor = sqlite_connection.cursor()
-#         print("Подключен к SQLite")
-#
-#         sql_select_query = """select * from words where id = ?"""
-#         if task_id == -1:
-#             task_id = random.randint(0, 261)
-#             cursor.execute(sql_select_query, (task_id,))
-#             records = cursor.fetchall()
-#             msg = ""
-#             curr_right_variant_id = "0"
-#             for row in records:
-#                 msg += "Task №" + str(row[0]) + "\nWord: " + row[1].lower() + "\n1) " + row[2] + "\n2) " + row[3]
-#                 if row[4] != "NULL":
-#                     msg += "\n3) " + row[4]
-#                 if row[5] != "NULL":
-#                     msg += "\n4) " + row[5]
-#                 curr_right_variant_id = row[6]
-#             cursor.close()
-#             return msg, curr_right_variant_id, task_id
-#         else:
-#             cursor.execute(sql_select_query, (task_id,))
-#             records = cursor.fetchall()
-#             msg = ""
-#             curr_right_variant_id = "0"
-#             for row in records:
-#                 msg += "Task №" + str(row[0]) + "\nWord: " + row[1].lower() + "\n1) " + row[2] + "\n2) " + row[3]
-#                 if row[4] != "NULL":
-#                     msg += "\n3) " + row[4]
-#                 if row[5] != "NULL":
-#                     msg += "\n4) " + row[5]
-#                 curr_right_variant_id = row[6]
-#             cursor.close()
-#             return msg, curr_right_variant_id, task_id
-#
-#     except sqlite3.Error as error:
-#         print("Ошибка при работе с SQLite", error)
-#     finally:
-#         if (sqlite_connection):
-#             sqlite_connection.close()
-#             print("Соединение с SQLite закрыто")
-#
-
-def user_data_create(username, user_id, task_status):
+def user_data_create(username, user_id):
     try:
         sqlite_connection = sqlite3.connect('sqlite_python.db')
         print("Подключен к SQLite user_data_create")
@@ -59,8 +14,8 @@ def user_data_create(username, user_id, task_status):
         for row in records:
             data += (row[0],)
         if user_id not in data:
-            data = (str(username),) + (str(user_id),) + (str(task_status),) + ('NULL',) + ('NULL',)
-            cursor.execute("INSERT INTO user_data VALUES(?, ?, ?, ?, ?);", data)
+            data = (str(username),) + (str(user_id),) + ('NULL',) + ('NULL',)
+            cursor.execute("INSERT INTO user_data VALUES(?, ?, ?, ?);", data)
             sqlite_connection.commit()
             cursor.close()
         else:
@@ -111,3 +66,23 @@ def get_user_data(user_id):
         if (sqlite_connection):
             sqlite_connection.close()
             print("Соединение с SQLite закрыто get_user_data")
+
+
+
+# def get_user_data(user_id):
+#     try:
+#         sqlite_connection = sqlite3.connect('sqlite_python.db')
+#         cursor = sqlite_connection.cursor()
+#         print("Подключен к SQLite get_user_data")
+#         sql_select_query = """select * from user_data where user_id = ?"""
+#         cursor.execute(sql_select_query, (str(user_id),))
+#         records = cursor.fetchall()
+#         print(records)
+#
+#
+#     except sqlite3.Error as error:
+#         print("Ошибка при работе с SQLite", error)
+#     finally:
+#         if (sqlite_connection):
+#             sqlite_connection.close()
+#             print("Соединение с SQLite закрыто get_user_data")
--- handlers/client.py
+++ handlers/client.py
@@ -1,4 +1,5 @@
 from aiogram import types, Dispatcher
+from aiogram.types import MediaGroup
 from create_bot import dp, bot
 from functions import *
 from aiogram.dispatcher import FSMContext
@@ -16,7 +17,7 @@ class FSMAdd(StatesGroup):
 async def process_start_command(message: types.Message,state: None):
     user_id = message.from_user.id
     username = message.from_user.username
-    user_data_create(username, user_id, False)
+    user_data_create(username, user_id)
     get_user_data(user_id)
     await FSMAdd.desc_ex.set()
     await bot.send_message(message.from_user.id,
@@ -33,19 +34,19 @@ async def process_decs_car_ex(message: types.Message, state: FSMContext):
 
 async def collect_data_with_car(message: types.Message, state: FSMContext):
     print('collect_data_with_car')
+    photoes = dict()
     try:
         async with state.proxy() as data:
-            print(1)
-            data['photo'] = message.media_group_id
-            print(data['photo'])
-            print(0)
-            await bot.send_message(message.from_user.id, "Вот для тебя пример описания")
+            data['photo'] = message.photo[0].file_id
+            photoes.append(data['photo'])
+            print(photoes)
             await message.reply("Спасибо за публикацию")
-            await bot.send_media_group(message.from_user.id, media=data)
+
             await FSMAdd.next()
     except:
         await message.reply("Прикрепи фото")
         dp.register_message_handler(collect_data_with_car, state=FSMAdd.collect_data_with_car)
+    await message.answer_media_group(media=photoes)
 
 
 async def process_send_to_moder(message: types.Message, state: FSMContext):
